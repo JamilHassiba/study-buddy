7510f33ae1b5ebaf2866eb2e5a5b52bb1b62ad85
Jamil Hassiba <jamil.hassiba@student.manchester.ac.uk>
2026-09-04 17:44:31 +0300

    Created db schema

---
+++ backend/schema.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE IF NOT EXISTS documents (
+    document_id SERIAL PRIMARY KEY,
+    file_name TEXT NOT NULL,
+    upload_date TIMESTAMPTZ DEFAULT NOW() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS chunks (
+    chunk_id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    embedding VECTOR(1536) NOT NULL,
+    document_id INT NOT NULL REFERENCES documents(document_id) ON DELETE CASCADE,
+    index INT NOT NULL
+);
+
+CREATE INDEX idx_chunks_document_id ON chunks(document_id);
