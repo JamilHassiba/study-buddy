2e975f0848c2d52db55d75e869d97970a2e466f1
Jamil Hassiba <jamil.hassiba@student.manchester.ac.uk>
2026-09-04 20:26:39 +0300

    Reformatted using Ruff and updated requirements

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,12 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
     database_url = os.getenv("DATABASE_URL")
+
 
 settings = Settings()
--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,11 @@
 from psycopg2 import pool
+
 from .config import settings
 
 connection_pool = pool.ThreadedConnectionPool(
-    minconn=1,
-    maxconn=10,
-    dsn=settings.database_url
+    minconn=1, maxconn=10, dsn=settings.database_url
 )
+
 
 def get_db():
     conn = connection_pool.getconn()
